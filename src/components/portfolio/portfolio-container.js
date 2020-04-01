7ea09be7bb8192d552a986b378258a0cb50675eb
EdwinTinoco <jaredlotic@gmail.com>
2020-04-01 16:50:14 -0600

    adding constructor in portfolioContainer class

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -3,8 +3,11 @@ import React, {Component} from 'react';
 import PortfolioItem from "./portfolio-item"
 
 export default class PortfolioContainer extends Component{   // Esta es una class base component
-    // State
-    // Lifecycle hooks
+    constructor(){
+        super();
+        
+        console.log("Portfolio container has rendered");
+    }
     render(){
         return (
             <div>
