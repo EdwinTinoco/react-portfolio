255d3663121c8675869b6433c94aec35eef889d7
EdwinTinoco <jaredlotic@gmail.com>
2020-04-08 15:39:29 -0600

    added no match page

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -3,9 +3,9 @@ import PortfolioContainer from '../portfolio/portfolio-container';
 
 export default function(){    
     return (
-    <div>
-        <h2>Homepage</h2>
-        <PortfolioContainer />
-    </div>
-    )
+        <div>
+            <h2>Homepage</h2>
+            <PortfolioContainer />
+        </div>
+    );
 }
