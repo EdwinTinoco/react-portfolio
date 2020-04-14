9fd1e32ae09369aaa4becf1b9c2bb07eee2b6de3
EdwinTinoco <jaredlotic@gmail.com>
2020-04-14 13:46:04 -0600

    finished styling the login page

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -5,14 +5,15 @@ export default class Auth extends Component{
     render(){
         return(
             <div className="auth-page-wrapper">
-                <div className="left-column" 
+                <div 
+                    className="left-column" 
                     style={{
                         backgroundImage: `url(${loginImg})`
                     }}
                 />
                 
-                <div className="left-right">
-                    <h1>Login component goes here</h1>
+                <div className="right-column">
+                    <h1>Login component goes here...</h1>
                 </div>                
             </div>
         );
