b5fe41ea5df0bf7c79c7e71e92dd137659d1c691
EdwinTinoco <jaredlotic@gmail.com>
2020-04-27 16:04:54 -0600

    styling the plus circle button to open the form modal

--- src/components/app.js
+++ src/components/app.js
@@ -7,7 +7,13 @@ import {
 import axios from "axios";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash, faSignOutAlt, faEdit, faSpinner } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTrash,
+  faSignOutAlt,
+  faEdit,
+  faSpinner,
+  faPlusCircle
+} from "@fortawesome/free-solid-svg-icons";
 
 import NavigationContainer from "./navigation/navigation-container"
 import Home from "./pages/home";
@@ -20,7 +26,7 @@ import PortfolioManager from "./pages/portfolio-manager";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
-library.add(faTrash, faSignOutAlt, faEdit, faSpinner);
+library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle);
 
 export default class App extends Component {
   constructor(props) {
