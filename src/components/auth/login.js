3a2345722026347a41955703d06aaa5fb9019b69
EdwinTinoco <jaredlotic@gmail.com>
2020-04-29 21:51:50 -0600

    styling auth form

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,8 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from "axios";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default class Login extends Component{
-    constructor(props){
+export default class Login extends Component {
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -21,28 +22,28 @@ export default class Login extends Component{
             errorText: ""
         });
     }
-    
+
     handleSubmit(event) {
         event.preventDefault();
 
         axios.post("https://api.devcamp.space/sessions",
-        {
-            client: {
-                email: this.state.email,
-                password: this.state.password
+            {
+                client: {
+                    email: this.state.email,
+                    password: this.state.password
+                }
+            },
+            {
+                withCredentials: true
             }
-        },
-        {
-            withCredentials: true
-        }
         ).then(response => {
-            if (response.data.status === "created"){
-                console.log('You can come in');  
-                this.props.handleSuccessfulAuth();              
-            }else{
+            if (response.data.status === "created") {
+                console.log('You can come in');
+                this.props.handleSuccessfulAuth();
+            } else {
                 this.setState({
                     errorText: "Wrong email or password"
-                    
+
                 })
             }
         }).catch(error => {
@@ -53,34 +54,38 @@ export default class Login extends Component{
         });
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
 
                 <div>{this.state.errorText}</div>
 
-                <form onSubmit={this.handleSubmit}>
-                    <input 
-                        type="email"
-                        name="email"
-                        placeholder="Your email"
-                        value={this.state.email}
-                        onChange={this.handleChange}
-                    />
-
-                    <input 
-                        type="password"
-                        name="password"
-                        placeholder="Your password"
-                        value={this.state.password}
-                        onChange={this.handleChange}
-                    />
-
-                    <div>
-                        <button type="submit">Login</button>
+                <form onSubmit={this.handleSubmit} className="auth-form-wrapper">
+                    <div className="form-group">
+                        <FontAwesomeIcon icon="envelope" />
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Your email"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                        />
                     </div>
-                </form>                
+
+                    <div className="form-group">
+                        <FontAwesomeIcon icon="lock" />
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Your password"
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                        />
+                    </div>
+
+                    <button className="btn" type="submit">Login</button>
+                </form>
             </div>
         );
     }
